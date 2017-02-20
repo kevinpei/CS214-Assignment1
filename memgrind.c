35344b3ed878ee16341978cdb7ac8409f398bb7f
kevinpei <kevinpei@users.noreply.github.com>
2017-02-19 19:00:45 -0500

    Added memgrind
    Still need to fix memgrind4

---
+++ memgrind.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int memgrind1() {
+	void* testArray[1000];
+	int i = 0;
+	while (i < 1000) {
+		testArray[i] = malloc(1);
+		printf("%d\t", i);
+		i++;
+	}
+	i = 0;
+	while (i < 1000) {
+		free(testArray[i]);
+		printf("%d\t", i);
+		i++;
+	}
+	return 0;
+}
+
+int memgrind2() {
+	int i = 0;
+	while (i < 1000) {
+		void* test = malloc(1);
+		free(test);
+		printf("%d\t", i);
+		i++;
+	}
+	return 0;
+}
+
+int memgrind3() {
+	int numMalloc = 0;
+	int numFree = 0;
+	void* testArray[1000];
+	while (numMalloc < 1000) {
+		if (rand() % 2 == 0) {
+			testArray[numMalloc] = malloc(1);
+			numMalloc++;
+		} else {
+			if (numFree < numMalloc) {
+				free(testArray[numFree]);
+				numFree++;
+			}
+		}
+	}
+	while (numFree < numMalloc) {
+		free(testArray[numFree]);
+		numFree++;
+	}
+	return 0;
+}
+
+int memgrind4() {
+	int numMalloc = 0;
+	int numFree = 0;
+	int sizeMalloc = 0;
+	void* testArray[1000];
+	while (numMalloc < 1000) {
+		if (rand() % 2 == 0 && sizeMalloc < 5000) {
+			int size = rand() % 64 + 1;
+			testArray[numMalloc] = malloc(size);
+			testArray[numMalloc] = &size;
+			numMalloc++;
+			sizeMalloc += size;
+			printf("%d\t", sizeMalloc);
+		} else {
+			if (numFree < numMalloc) {
+				sizeMalloc -= *testArray[numFree];
+				free(testArray[numFree]);
+				numFree++;
+				printf("%d\t", sizeMalloc);
+			}
+		}
+	}
+	while (numFree < numMalloc) {
+		free(testArray[numFree]);
+		numFree++;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	memgrind1();
+	memgrind2();
+	memgrind3();
+	memgrind4();
+	return 0;
+}
