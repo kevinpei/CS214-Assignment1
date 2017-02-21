6e26dddff76733cdc422f2c75b650b915722665e
kzhang098 <kz100@scarletmail.rutgers.edu>
2017-02-21 13:07:44 -0500

    Add files via upload

--- mymalloc.c
+++ mymalloc.c
@@ -22,13 +22,13 @@ boolean initialize() {
         mainMemory->size = 5000 - sizeof(MemoryData); //The size of the memory that is available left for uses is this size    
 	mainMemory->isFree = '1'; //
 	mainMemory->startOfData = memoryblock;
- 
+
 	return 1;
 }
 
 //This method will help with finding the first free 
 
-MemoryData* findFirstFree(int size,MemoryData * start) {
+MemoryData* findFirstFree(int size, MemoryData * start) {
 	MemoryData * ptr = start;
 	//Iterate through the memory blocks until you find a block that's both free and can fit in the memory we want to malloc, plus its metadata
 	while ((start->isFree == FALSE || ptr->size < size + sizeof(MemoryData)) && ptr != NULL) {
@@ -44,33 +44,48 @@ void * mymalloc(int size, char* myfile, int line) {
 		printf("You have attempted to allocate a non-positive number bytes in File: '%s' Line: '%d'\n", myfile, line); 
 		return 0;
 	}	
+	
+	printf("%s\n", "Size is enough");
 
-	if(memInit != 1) {
+	if(memInit == FALSE) {
+		printf("%s\n", "Initializing memory...");
 		initialize(); 
 		firstFreeAddress = mainMemory;
+		memInit = TRUE;
 	} 
 	
 	
-	MemoryData* firstFreeAddress = findFirstFree(size,firstFreeAddress); 
-
+	firstFreeAddress = findFirstFree(size,firstFreeAddress); 
+	
+	printf("%s\n", "Found first free address");
+		
 	if(firstFreeAddress != NULL) {  // This means that we have enough space in "main memory" to allocate
 		//Set new memory location for free memory
+		
+		printf("%s\n", "Found address is not null...");
+
 
 		MemoryData* newFree = (MemoryData *)(firstFreeAddress->size + sizeof(MemoryData) + size); //Keeps track of free index
 		newFree->size = mainMemory - sizeof(MemoryData) - size; //This keeps track of how much memory is free
 		newFree->isFree = TRUE; 
 		newFree->next = firstFreeAddress->next;			
-			
+		
+		printf("%s\n", "Created new object and metadata...");
+		
 		//Now I have to change the values for the data I allocated to something.
 		firstFreeAddress->size = size;
 		firstFreeAddress->isFree = FALSE; 
 		firstFreeAddress->next = newFree;
-		
-		MemoryData test = *(firstFreeAddress + 1);
+
+		printf("%s\n", "Pointing to new object and metadata");
+			
+		MemoryData test = *(firstFreeAddress);
 	
 		printf("%d\n", test.size);
 		printf("%d\n", test.isFree); 
 		
+		printf("%s\n", "HOME STRETCH!!!");
+
 		return firstFreeAddress + 1;
 	} else {
 		printf("There is not enough space in memory in order to allocated the amount requested in File: '%s' Line: '%d'\n", myfile, line);
---
+++ test.c
@@ -0,0 +1,9 @@
+#include "mymalloc.h"
+
+int main(int argc, char ** argv)  {
+	
+	int * x;
+	x = (int *)malloc(1); 
+		
+	return 0; 
+}
